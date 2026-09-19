a813ac78c934c0e5e0491ef32f878354ac460c14
nguyennghia0479 <nguyennghhia0479@gmail.com>
2026-09-19 17:30:40 +0700

    Add mobile modified

--- Assets/_Project/Scripts/UIs/TutorialUI.cs
+++ Assets/_Project/Scripts/UIs/TutorialUI.cs
@@ -2,4 +2,17 @@ using UnityEngine;
 
 public class TutorialUI : MonoBehaviour
 {
+    [SerializeField] private GameObject webGuide;
+    [SerializeField] private GameObject mobileGuide;
+
+    private void Awake()
+    {
+        webGuide.SetActive(true);
+        mobileGuide.SetActive(false);
+
+#if UNITY_ANDROID
+        webGuide.SetActive(false);
+        mobileGuide.SetActive(true);
+#endif
+    }
 }
