0d47ff01855322519dad263173ea68cab49d73d9
nguyennghia0479 <nguyennghhia0479@gmail.com>
2026-09-21 08:47:49 +0700

    Add Google mobile ads

--- Assets/_Project/Scripts/UIs/CompleteUI.cs
+++ Assets/_Project/Scripts/UIs/CompleteUI.cs
@@ -18,19 +18,27 @@ public class CompleteUI : MonoBehaviour
     [SerializeField] private Button retryBtn;
 
     [Header("Localization Elements")]
-    [SerializeField] protected string tableReference;
+    [SerializeField] private string tableReference;
     [SerializeField] private string messageKey;
     [SerializeField] private string nextStageKey;
+
+    [Space]
+    [SerializeField] private InterstitialAdManager adManager;
 
     private LocalizedString messageLocalized;
     private LocalizedString nextStageLocalized;
     private int moves;
     private string nextStageName;
+    private bool isMobilePlatform;
 
     private void Awake()
     {
         messageLocalized = new(tableReference, messageKey);
         nextStageLocalized = new(tableReference, nextStageKey);
+        isMobilePlatform = false;
+#if UNITY_ANDROID
+        isMobilePlatform = true;
+#endif
     }
 
     private void OnEnable()
@@ -86,6 +94,14 @@ public class CompleteUI : MonoBehaviour
     private void UpdateMessageText(string value) => messageText.text = string.Format(value, moves);
 
     private void OnNextLevelButtonClicked()
+    {
+        if (isMobilePlatform && adManager != null)
+            adManager.ShowAd(ProceedAfterAd);
+        else
+            ProceedAfterAd();
+    }
+
+    private void ProceedAfterAd()
     {
         if (LevelManager.Instance.CanLoadNextStage || !LevelManager.Instance.IsLastLevel)
             UIEvents.RaiseNextLevelButtonClicked();
